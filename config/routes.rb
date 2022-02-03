fd278ed0f72c454b63374a3d74d96c29f34fb799
Ely Sanneman <65604667+thisguyEly@users.noreply.github.com>
2022-02-03 10:03:56 -0500

    Add files via upload

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root "articles#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "articles#index"
+  # Defines the root path route ("/")
+end
